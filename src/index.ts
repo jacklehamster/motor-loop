77e3273be23f77d8b17ead8ac2b33c8d668ea84b
Jack Le Hamster <jacklehamster@gmail.com>
2024-01-28 19:36:32 -0800

    Auto-commit: 2024-01-28 19:36:32
    Files:\npackage.json\nsrc/index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { IMotor } from "./motor/IMotor";
 import { Looper } from "./motor/Looper";
 import { Priority } from "./motor/Priority";
 import { Time } from "./motor/Time";
+import { UpdatePayload } from "./motor/update/UpdatePayload";
 
-export { Motor, IMotor, Looper, Priority, Time };
+export { Motor, IMotor, Looper, Priority, Time, UpdatePayload };
