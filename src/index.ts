fadc62fb16231f05c9b0bada0d632d52fa588f65
Jack Le Hamster <jacklehamster@gmail.com>
2024-01-28 20:13:23 -0800

    Auto-commit: 2024-01-28 20:13:23
    Files:\nsrc/index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import { Looper } from "./motor/Looper";
 import { Priority } from "./motor/Priority";
 import { Time } from "./motor/Time";
 import { UpdatePayload } from "./motor/update/UpdatePayload";
+import { Refresh } from "./motor/update/Refresh";
 
-export { Motor, IMotor, Looper, Priority, Time, UpdatePayload };
+export { Motor, IMotor, Looper, Priority, Time, UpdatePayload, Refresh };
