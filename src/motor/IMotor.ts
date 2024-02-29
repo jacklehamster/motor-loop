cd648272eb97fc7b2f0d9c663bfce9d24dd8fdea
Jack Le Hamster <jacklehamster@gmail.com>
2024-02-29 13:20:52 -0800

    Auto-commit: 2024-02-29 13:20:52
    Files:
    bun.lockb
    package.json
    src/motor/IMotor.ts
    
    https://github.com/jacklehamster/autocommit

--- src/motor/IMotor.ts
+++ src/motor/IMotor.ts
@@ -1,7 +1,8 @@
 import { Cycle } from "./update/Cycle";
 import { ITimeProvider } from "./Time";
+import { Active } from "dok-types";
 
-export interface IMotor extends ITimeProvider {
+export interface IMotor extends ITimeProvider, Active {
   loop<T>(cycle: Cycle<T>, data: T, frameRate?: number): void;
   scheduleUpdate<T>(cycle: Cycle<T>, data?: T, refreshRate?: number, future?: boolean): void;
   stopUpdate<T>(cycle: Cycle<T>): void;
